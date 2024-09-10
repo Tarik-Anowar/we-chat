cda1e340b867c417e976d73df54f6a69842a7f2a
Tarik Anowar <itarikju@gmail.com>
2024-09-11 03:04:28 +0530

    added github workflow

--- apps/web/app/api/sign-image/route.ts
+++ apps/web/app/api/sign-image/route.ts
@@ -4,6 +4,6 @@ export async function POST(request:Request) {
     const body = (await request.json()) as {paramsToSign:Record<string,string>};
     const {paramsToSign} = body;
 
-    const signature = cloudinary.utils.api_sign_request(paramsToSign,process.env.CLOUDINARY_API_SECRET as string);
+    const signature = cloudinary.utils.api_sign_request(paramsToSign,process.env.CLOUDINARY_API_SECRET as string) || "";
     return Response.json({signature});
 }
